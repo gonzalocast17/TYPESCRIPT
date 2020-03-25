cd38ee9aebe236635596fcc5757a8eb1a5c91cd5
gonzalocast17 <gonzalocast17@gmail.com>
2020-03-25 01:12:47 -0600

    Creación de "Hola mundo" y compilación a javascript

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,17 @@
+System.register("index", [], function (exports_1, context_1) {
+    "use strict";
+    var world;
+    var __moduleName = context_1 && context_1.id;
+    function hello(word) {
+        if (word === void 0) { word = world; }
+        return "Hello " + world + "! ";
+    }
+    exports_1("hello", hello);
+    return {
+        setters: [],
+        execute: function () {
+            world = '🗺️';
+        }
+    };
+});
 //# sourceMappingURL=index.js.map
---
+++ src/app/components/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Ejemplo de funcion inicial
+ */
+
+// Declaración de variable const
+const world = '🗺️';
+// Declaración de exportación de funcion
+export function hello(word: string = world): string {
+    return `Hello ${world}! `;
+  }

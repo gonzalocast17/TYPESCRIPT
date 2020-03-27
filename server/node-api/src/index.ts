106b0a1cdacd0fc8018b6cc83197ba3dc8b3bc80
gonzalocast17 <gonzalocast17@gmail.com>
2020-03-26 23:40:11 -0600

    Instalación y configuración inicial de Loopback

---
+++ server/node-api/src/index.ts
@@ -0,0 +1,16 @@
+import {AplicacionInicial} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {AplicacionInicial};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new AplicacionInicial(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
